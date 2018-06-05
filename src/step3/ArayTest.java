c4db2f57af299e543d4b3411841145023b8414df
Lee Seul <lina900904@gmail.com>
2018-06-05 21:01:10 +0900

    java 폴더추가

---
+++ src/step3/ArayTest.java
@@ -0,0 +1,73 @@
+package step3;
+
+import javax.swing.JOptionPane;
+//총점, 평균,등급
+
+public class ArayTest {
+
+	public static int gettotal(String[] scorses) {
+		return Integer.parseInt(scorses[1]) + Integer.parseInt(scorses[2]) + Integer.parseInt(scorses[3]);
+	}
+
+	public static int getavg(String[] scorses) {
+		return (Integer.parseInt(scorses[1]) + Integer.parseInt(scorses[2]) + Integer.parseInt(scorses[3])) / 3;
+	}
+
+	public static void main(String[] args) {
+		String[] students = new String[5];
+		String[] ranking = new String[3];
+	
+		int count=0;
+		while (true) {
+		
+			String menu = JOptionPane.showInputDialog("0.종료 1.성적입력 2.전체보기 3.순위출력");
+			switch (menu) {
+			case "0":
+				return;
+			case "1":
+			
+				String score = JOptionPane.showInputDialog("이름/국어/영어/수학");
+				String[] arr = score.split("/");
+				int total =gettotal(arr);
+				int avg=getavg(arr);
+				
+					String student = arr[0] + "|" + arr[1] + "|" + arr[2] + "|" + arr[3] + "|" +total + "|"
+							+ avg;
+					students[count] = student;
+					count++;
+				
+
+				break;
+				case "2":
+					String result = "순위|이름|국어|영어|수학|총점|평균\n";
+					for(int i=0;i<students.length;i++) {
+						result+=students[i]+"\n";
+					}
+					JOptionPane.showMessageDialog(null,	 result);
+					break;
+					
+				case "3":
+					String[] temp = new String[4];
+			/*	
+					temp[0] = students[0].split("\\|");
+					int a = Integer.parseInt(temp[4]);
+					temp[1] = students[1].split("\\|");
+					int b = Integer.parseInt(temp[4]);
+					temp[2] = students[2].split("\\|");
+					int c = Integer.parseInt(temp[4]);
+					if(a>b&&a>c) {
+						if(b>c) {
+							ranking[0]=students[0];//a>b>c
+						}else {
+							ranking[1]=students[2];//a>c>b
+						}*/
+				/*	}else if(b>c) {*/
+					JOptionPane.showMessageDialog(null,ranking[0]+ranking[1]+ranking[2]);
+				break;
+			
+			}
+			}
+
+		}
+
+}
--- src/step3/Note.java
+++ src/step3/Note.java
@@ -6,17 +6,43 @@ public class Note {
 
 	public static void main(String[] args) {
 
-		String name = JOptionPane.showInputDialog("이름을 입력하세요");
-		int price = Integer.parseInt(JOptionPane.showInputDialog("출장비 지급액을 입력하세요"));
-		int[] arr = {50000, 10000,5000,1000,500,100,50,10,5};
-		
-		
-		JOptionPane.showMessageDialog(null,"성명:"+name
-					+"출장비지급액:"+price+"오만:");
-
 
 		
-
+		
+		
+		while (true) {
+			String name = JOptionPane.showInputDialog("이름을 입력하세요(QUIT입력시 종료)");
+			int[] note = new int[10];
+			int[] result = new int[10];
+			int[] total = new int[10];
+			switch(name) {
+			case "QUIT": 
+				return;
+			default :
+				String price = JOptionPane.showInputDialog("출장비 지급액을 입력하세요");
+				
+	
+				int[] arr = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5};
+				int[] num = { 5, 2, 5, 2, 5, 2, 5, 2, 5 };
+				for(int i=0; i<=arr.length;i++) {
+					
+				
+					total[i] = Integer.parseInt(price)%arr[0];
+					note[i] = total[i]/arr[i];
+					result[i] = Integer.parseInt(price)/arr[i];		//갯수		
+					
+					
+					JOptionPane.showMessageDialog(null, "성명:  " + name + "출장비지급액: "+price+"\n"+ "오만:"+result[0]
+							+"\n일만: "+ note[1]);
+				 break;
+				}
+				
+			}	
+		
+		}
+			
+		
+			
 		
 		
 		
